937f62d55f2731a020cba85876abede24a6611b5
Sean Welsh Brown <seanwelshbrown@gmail.com>
2022-10-02 23:20:30 -0400

    Accidentally forgot to save main file when staging for previous commit

--- snowman.py
+++ snowman.py
@@ -24,6 +24,9 @@ def find_and_replace_letters(letter, word, word_in_progress):
 
     return " ".join(word_arr)
 
+def bold_print(str, color=None):
+    cprint(str, color, attrs=["bold"])
+
 def intro_message(word_in_progress):
     print()
     cprint(figlet_format('Snowman', font='big'), "blue", attrs=['bold'])
@@ -33,8 +36,19 @@ def intro_message(word_in_progress):
     bold_print(word_in_progress)
     print()
 
-def bold_print(str, color=None):
-    cprint(str, color, attrs=["bold"])
+def game_end(win_or_loss):
+    if win_or_loss == "win":
+        print("You guessed the word!\n")
+    else:
+        print("You didn't guess the word, and now the evil Snowman is on the loose! 😱")
+    
+    
+    print(f"The word was '{word.capitalize()}'\n")
+
+    if input("Play again? [Y / N]: ").upper() == "Y":
+        snowman()
+    else:
+        print("Thanks for playing!\n")
 
 
 #~~ MAIN GAME ~~#
@@ -43,14 +57,20 @@ def snowman():
     word = get_word(words)
     word_in_progress = "_ " * len(word)
     wrong_letters = set()
-    num_of_guesses = 0
+    num_of_wrong_guesses = 0
 
     intro_message(word_in_progress)
     guess = input("Guess a letter to get started: ").upper()
     print()
 
-    while "_" in word_in_progress or num_of_guesses < 8:
+    while True:
         os.system('clear')
+
+        # Win/Loss conditions
+        if "_" not in word_in_progress:
+            game_end("win")
+        elif num_of_wrong_guesses == 8:
+            game_end("loss")
 
         # Handle invalid guesses
         if len(guess) > 1:
@@ -69,22 +89,13 @@ def snowman():
                 bold_print("Sorry, that letter isn't in the word. Try again!\n", "red")
                 wrong_letters.add(guess)
         
-        draw_snowman(len(wrong_letters))
+        draw_snowman(num_of_wrong_guesses)
         bold_print(word_in_progress)
         print()
         print()
         cprint("Wrong letters: \n{wrong_letters}\n".format(wrong_letters = ', '.join(wrong_letters)), "magenta", attrs=["bold"])
         guess = input("Guess another letter: ").upper()
         print()
-
-    print()
-    print("You guessed the word!\n")
-    print(f"The word was '{word.capitalize()}'\n")
-
-    if input("Play again? [Y / N]: ").upper() == "Y":
-        snowman()
-    else:
-        print("Thanks for playing!\n")
 
 
 ## RUN THE PROGRAM ##
